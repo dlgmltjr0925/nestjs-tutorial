94b96453e18c8df9ca8081b080c1716b03dc2d45
fine <dlgmltjr0925@gmail.com>
2021-01-30 00:41:28 +0900

    Controllers - Status code

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
-
-import { Request } from 'express';
+import { Controller, Get, HttpCode, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Post()
+  @HttpCode(204)
   create(): string {
     return 'This action adds a new cat';
   }
