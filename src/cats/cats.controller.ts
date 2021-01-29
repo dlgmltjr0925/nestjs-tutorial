bfce5ae0caaa82b767937cfc3e4b8cd66c242aec
fine <dlgmltjr0925@gmail.com>
2021-01-30 00:42:14 +0900

    Controllers - Headers

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { Controller, Get, Header, Post } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  @HttpCode(204)
+  @Header('Cache-Control', 'none')
   create(): string {
     return 'This action adds a new cat';
   }
