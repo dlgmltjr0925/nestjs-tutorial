f543e8c8ce1444c1bbd6eceeb5354a5f849685ee
fine <dlgmltjr0925@gmail.com>
2021-01-31 22:32:49 +0900

    Controllers - Library specific approach

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
+import { AccountController } from './account/account.controller';
+import { AdminController } from './admin/admin.controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { Module } from '@nestjs/common';
-import { AdminController } from './admin/admin.controller';
-import { AccountController } from './account/account.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, AdminController, AccountController],
+  controllers: [
+    AppController,
+    CatsController,
+    AdminController,
+    AccountController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,23 +3,30 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Post,
   Put,
   Query,
+  Res,
 } from '@nestjs/common';
 import { CreateCatDto, UpdateCatDto, ListAllEntities } from './dto';
+import { Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+  create(@Body() createCatDto: CreateCatDto, @Res() res: Response) {
+    res.status(HttpStatus.CREATED).send();
   }
 
   @Get()
-  findAll(@Query() query: ListAllEntities) {
-    return `This action returns all cats (limit: ${query.limit} items)`;
+  findAll(
+    @Query() query: ListAllEntities,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    res.status(HttpStatus.OK);
+    return [];
   }
 
   @Get(':id')
