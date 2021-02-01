a4237a38fa308d75c5b30eea62cf1a1fc2766583
fine <dlgmltjr0925@gmail.com>
2021-02-02 01:09:29 +0900

    Middleware - Global middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
+import { logger } from './common/middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(logger);
   await app.listen(3000);
 }
 bootstrap();
