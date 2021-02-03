aff00505b2210dcfa06645368efabf786541727e
fine <dlgmltjr0925@gmail.com>
2021-02-03 10:22:31 +0900

    Exception filters - Inheritance

--- src/common/exception/all-exceptions.filter.ts
+++ src/common/exception/all-exceptions.filter.ts
@@ -1,25 +1,36 @@
 import {
   ArgumentsHost,
+  Catch,
   ExceptionFilter,
   HttpException,
+  HttpServer,
   HttpStatus,
 } from '@nestjs/common';
 
-export class AllExceptionsFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getRequest();
-    const request = ctx.getRequest();
+import { BaseExceptionFilter } from '@nestjs/core';
 
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : HttpStatus.INTERNAL_SERVER_ERROR;
+// export class AllExceptionsFilter implements ExceptionFilter {
+//   catch(exception: any, host: ArgumentsHost) {
+//     const ctx = host.switchToHttp();
+//     const response = ctx.getRequest();
+//     const request = ctx.getRequest();
 
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-    });
+//     const status =
+//       exception instanceof HttpException
+//         ? exception.getStatus()
+//         : HttpStatus.INTERNAL_SERVER_ERROR;
+
+//     response.status(status).json({
+//       statusCode: status,
+//       timestamp: new Date().toISOString(),
+//       path: request.url,
+//     });
+//   }
+// }
+
+@Catch()
+export class AllExceptionsFilter extends BaseExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    super.catch(exception, host);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+
+import { AllExceptionsFilter } from './common/exception/all-exceptions.filter';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exception/http-exception.filter';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { NestFactory } from '@nestjs/core';
 import { logger } from './common/middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(logger);
   // app.useGlobalFilters(new HttpExceptionFilter());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
