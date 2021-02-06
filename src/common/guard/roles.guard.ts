af4850dd32f679b5bf1714cb3b371d74ec31f9ee
fine <dlgmltjr0925@gmail.com>
2021-02-06 14:20:31 +0900

    Guards - Putting it all together

--- src/common/guard/roles.guard.ts
+++ src/common/guard/roles.guard.ts
@@ -1,12 +1,22 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+
+const matchRoles = (roles: string[], userRoles: string[]) => {
+  return true;
+};
 
 @Injectable()
 export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) return true;
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return matchRoles(roles, user.roles);
   }
 }
