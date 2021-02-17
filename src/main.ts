3025052d8e645ab661ebf5c945484737ce164936
fine <dlgmltjr0925@gmail.com>
2021-02-17 18:12:16 +0900

    Security - Helmet

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as helmet from 'helmet';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,6 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
