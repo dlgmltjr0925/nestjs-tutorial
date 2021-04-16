231e4eee24ef08eab863a9f8607ebb573626ee6e
fine <dlgmltjr0925@gmail.com>
2021-04-16 12:06:39 +0900

    Initialize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,41 +1,10 @@
-import { APP_GUARD } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AuthModule } from './auth/auth.module';
-import { CatsController } from './cats/cats.controller';
-import { CatsModule } from './cats/cats.module';
-import { CatsService } from './cats/cats.service';
-import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
-import { RecipesModule } from './recipes/recipes.module';
-import { RolesGuard } from './auth/roles.guard';
-import { UsersModule } from './users/users.module';
-import { CaslModule } from './casl/casl.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    RecipesModule,
-    GraphQLModule.forRoot({
-      installSubscriptionHandlers: true,
-      autoSchemaFile: 'schema.gql',
-    }),
-    AuthModule,
-    UsersModule,
-    CatsModule,
-    CaslModule,
-  ],
-  // providers: [
-  //   {
-  //     provide: APP_GUARD,
-  //     useClass: JwtAuthGuard,
-  //   },
-  // ],
-  controllers: [AppController, CatsController],
-  providers: [
-    CatsService,
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    },
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/casl/casl.module.ts
+++
@@ -1,8 +0,0 @@
-import { CaslAbilityFactory } from './casl-ability.factory';
-import { Module } from '@nestjs/common';
-
-@Module({
-  providers: [CaslAbilityFactory],
-  exports: [CaslAbilityFactory],
-})
-export class CaslModule {}
